4a76eefe124a2de7a59bb3bee658a321bd7d77a0
joesan <codeintheopen@gmail.com>
2020-07-27 18:59:22 +0200

    added new variables

--- azure/main.tf
+++ azure/main.tf
@@ -1,26 +1,34 @@
 provider "azurerm" {
-  version = "=2.13.0"
-
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  
   features {}
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.prefix}-kubernetes-resources"
-  location = "uksouth"
+  name     = var.prefix
+  location = var.location
 }
 
 resource "azurerm_kubernetes_cluster" "cluster" {
-  name       = "${var.prefix}-kubernetes-cluster"
+  name       = var.prefix
   location   = azurerm_resource_group.rg.location
-  dns_prefix = "${var.prefix}-k8s"
+  dns_prefix = var.prefix
 
   resource_group_name = azurerm_resource_group.rg.name
-  kubernetes_version  = "${var.k8s-version}"
+  kubernetes_version  = "var.k8s_version"
 
   default_node_pool {
-    name       = "aks"
-    node_count = "${default-node-count}"
-    vm_size    = "${default-vm-size}"
+    name       = "${var.prefix}_aks"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+  }
+
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
   }
 
   identity {
--- azure/variables.tf
+++ azure/variables.tf
@@ -1,19 +1,42 @@
+variable "subscription_id" {
+  default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
+
+variable "tenant_id" {
+  default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
+
 variable "prefix" {
+  type = string
   description = "A prefix used for all resources in this example"
+  default = "joesan"
+}
+
+variable "client_id" {
+  description = "The Client ID (appId) for the Service Principal used for the AKS deployment"
+}
+
+variable "client_secret" {
+  description = "The Client Secret (password) for the Service Principal used for the AKS deployment"
 }
 
 variable "location" {
+  type = string
   description = "The Azure Region in which all resources in this example should be provisioned"
+  default = "westeurope"
 }
 
-variable "k8s-version" {
+variable "k8s_version" {
   description = "The version of the Kubernetes to be used"
+  default = "1.2.3"
 }
 
-variable "default-vm-size" {
+variable "vm_size" {
   description = "The size of the vm for our default pool"
+  default = "Standard_D2s_v3"
 }
 
-variable "default-node-count" {
+variable "node_count" {
   description = "The count of the vm nodes for our default pool"
+  default = 2
 }
